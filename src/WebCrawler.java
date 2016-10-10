87aeed702e28bfb8eb190426bee74715c3f2e2d4
HikingBe4r <kimisa0927@gmail.com>
2016-10-10 10:59:04 +0900

    주석 수정

--- src/WebCrawler.java
+++ src/WebCrawler.java
@@ -31,60 +31,61 @@ public class WebCrawler {
 	}
 
 	public static void main(String[] args) throws ClientProtocolException, IOException {
-		int i = 1;
+		 int i = 1;
 		 String Url1 = "http://m.hungryapp.co.kr/bbs/game_7knights_charDetail.php?no=";
 	     String Url2 = "&page=1";
 	     String Url3;
-		while(i < 2){
-			// 2. 가져올 HTTP 주소 세팅
-			Url3 = Url1 + i + Url2;
-		   HttpPost http = new HttpPost(Url3);
+	     
+	     // 모든 영웅목록 반복문 실행. [161010] 현재 427개 있음.
+	     while(i < 2){
+			//  HTTP 주소 세팅
+	    	Url3 = Url1 + i + Url2;
+			HttpPost http = new HttpPost(Url3);
 	
-		   // 3. 가져오기를 실행할 클라이언트 객체 생성
-		   HttpClient httpClient = HttpClientBuilder.create().build();
+			// 가져오기를 실행할 클라이언트 객체 생성
+			HttpClient httpClient = HttpClientBuilder.create().build();
 	
-		   // 4. 실행 및 실행 데이터를 Response 객체에 담음
-		   HttpResponse response = httpClient.execute(http);
+			// 실행 및 실행 데이터를 Response 객체에 담음
+			HttpResponse response = httpClient.execute(http);
 	
-		   // 5. Response 받은 데이터 중, DOM 데이터를 가져와 Entity에 담음
-		   HttpEntity entity = response.getEntity();
+			// Response 받은 데이터 중, DOM 데이터를 가져와 Entity에 담음
+			HttpEntity entity = response.getEntity();
 		   
-		   // 6. Charset을 알아내기 위해 DOM의 컨텐트 타입을 가져와 담고 Charset을 가져옴 
-		   ContentType contentType = ContentType.getOrDefault(entity);
-	       Charset charset = contentType.getCharset();
+			// Charset을 알아내기 위해 DOM의 컨텐트 타입을 가져와 담고 Charset을 가져옴 
+			ContentType contentType = ContentType.getOrDefault(entity);
+			Charset charset = contentType.getCharset();
 	        
-	       // 7. DOM 데이터를 한 줄씩 읽기 위해 Reader에 담음 (InputStream / Buffered 중 선택은 개인취향) 
-		   BufferedReader br = new BufferedReader(new InputStreamReader(entity.getContent(), charset));
+			// DOM 데이터를 한 줄씩 읽기 위해 Reader에 담음 (InputStream / Buffered 중 선택은 개인취향) 
+			BufferedReader br = new BufferedReader(new InputStreamReader(entity.getContent(), charset));
 		   
 	
-		   // 8. 가져온 DOM 데이터를 담기위한 그릇
-		   StringBuffer sb = new StringBuffer();
+			// 가져온 DOM 데이터를 담기위한 그릇
+			StringBuffer sb = new StringBuffer();
 	
-		   // 9. DOM 데이터 가져오기
-		   String line = "";
-		   int count = 0;
-		   while((line=br.readLine()) != null){
-			   if (count > 1250 && count < 1387){
-				   sb.append(line+"\n");   
-			   } else if(count > 1500){
-				   break;			   
-			   }
-			   count++;
-		   }
-		   // 10. 가져온 아름다운 DOM을 보자
-		   System.out.println(sb.toString());
+			// DOM 데이터 가져오기
+			String line = "";
+			int count = 0;
+			while((line=br.readLine()) != null){
+				if (count > 1250 && count < 1387){
+					sb.append(line+"\n");   
+				} else if(count > 1500){
+					break;			   
+				}
+				count++;
+			}
+			// 가져온 아름다운 DOM을 보자
+			System.out.println(sb.toString());
 		   
-		   // ** txt파일로 저장해보자
-		   BufferedWriter txtCreater;
+			// ** txt파일로 저장해보자
+			BufferedWriter txtCreater;
 		   
-		   String filename = "C:/Users/HikingBear/Documents/webcrawler/WebCrawler/etc" + "webcrawler_" + i + ".txt";
+			String filename = "C:/Users/HikingBear/Documents/webcrawler/WebCrawler/etc/really/" + "webcrawler_" + i + ".html";
 		   
+			txtCreater = new BufferedWriter(new FileWriter(filename));
+			txtCreater.write(sb.toString()); txtCreater.newLine();
+			txtCreater.close();
 		   
-		   txtCreater = new BufferedWriter(new FileWriter(filename));
-		   txtCreater.write(sb.toString()); txtCreater.newLine();
-		   txtCreater.close();
-		   
-		   i++;
+			i++;
 		}
 	}
 
